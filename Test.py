f5b234170aa396cfc7f72af9ad058e8482c4b011
SGBye <ctac1995@gmail.com>
2018-10-13 00:52:01 +0300

    Second update

--- Test.py
+++ Test.py
@@ -6,15 +6,12 @@ from getpass import getpass
 URL = sys.argv[1]
 Tries = int(sys.argv[2])
 TotalPing = 0
-PingList = []
 
 for i in range(Tries):
-    PingList.append(requests.get(URL).elapsed.total_seconds())
     TotalPing += requests.get(URL).elapsed.total_seconds()
 
 AveragePing = TotalPing / Tries
-print(getpass())
-sk = Skype("xx4epTuKxx", getpass())
 
-ch = sk.contacts["live:ctac1995_2"].chat
+sk = Skype("ctac1995@gmail.com", getpass())
+ch = sk.contacts["xx4eptukxx"].chat
 ch.sendMsg("Средняя задержка отклика с сайта %s составила %f" % (URL, AveragePing))
